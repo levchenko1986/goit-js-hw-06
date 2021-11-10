86ffccf410a5c20c426c60a1e53d0458e36488d7
levchenko1986 <89630449+levchenko1986@users.noreply.github.com>
2021-11-10 15:39:18 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const totalCategories = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${totalCategories.length}`);
 
 const items = document.querySelectorAll('li.item');
-Array.prototype.forEach.call(items, (element) => {
+items.forEach((element) => {
     const title = element.querySelector('h2').innerHTML;
     const itemsLength = element.querySelectorAll('li').length;
     console.log(`Category: ${title} 
